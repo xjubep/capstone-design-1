4053226c4fbe1822ecbea76795c60f54ae157268
xjubep <xjubep@gmail.com>
2020-03-23 13:41:21 +0000

    apply tz KST

--- week2/web/server.js
+++ week2/web/server.js
@@ -1,19 +1,22 @@
 var express = require('express');
 var app = express();
 var port = 8000;
-var dateUtils = require('date-utils');
+
+var moment = require('moment');
+require('moment-timezone');
+moment.tz.setDefault("Asia/Seoul");
+
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-var time = new Date();
-
 app.get('/get', function (req, res) {
     var r = req.query;
+    var date = moment();
 
     r.email = "xjubep@gmail.com";
     r.stuno = "20141496";
-    r.time = time.toFormat("YYYY-MM-DD HH:MI:SS");
+    r.time = date.format('YYYY-MM-DD hh:mm:ss');
     r.ip = req.ip.replace(/^.*:/, '');
     console.log(r);
     res.send(JSON.stringify(r));
@@ -21,12 +24,12 @@ app.get('/get', function (req, res) {
 
 app.post('/', function (req, res) {
     var r = req.body;
+    var date = moment();
 
     r.email = "xjubep@gmail.com";
     r.stuno = "20141496";
-    r.time = time.toFormat("YYYY-MM-DD HH:MI:SS");
+    r.time = date.format('YYYY-MM-DD hh:mm:ss');
     r.ip = req.ip.replace(/^.*:/, '');
-    console.log(req.body);
     res.send(JSON.stringify(r));
 });
 
